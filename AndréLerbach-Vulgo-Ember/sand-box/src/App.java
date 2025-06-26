1df2ef3d82cb8c527042c4ea12a5856747d43e25
Pedro luca Macedo <pedrolucamacedo@MacBook-Air-de-Pedro.local>
2025-06-25 22:08:03 -0300

    Salvando mudanças antes do rebase

--- AndréLerbach-Vulgo-Ember/sand-box/src/App.java
+++ AndréLerbach-Vulgo-Ember/sand-box/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
+<<<<<<< HEAD
         System.out.println("Olá, Programação");
+=======
+        System.out.println("ALOHA");
+>>>>>>> c0d9003 (Adiciona minha pasta com os exercícios)
     }
 }
--- Professor/sand-box/src/App.java
+++ Professor/sand-box/src/App.java
@@ -1,5 +1,34 @@
+<<<<<<< HEAD
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá OO");
+=======
+
+import pokemon.Luta;
+import pokemon.Pokemon;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+     Pokemon pikachu = new Pokemon("Pikachu", 3);
+     Pokemon pikachu2 = new Pokemon("Pikachu", 3);
+     Pokemon charmander = new Pokemon("Charmander", 4); 
+     
+     Luta luta = new Luta(pikachu, charmander);
+     Luta luta2 = new Luta(pikachu, pikachu2);
+        
+     luta.iniciarLuta();
+     luta2.iniciarLuta();
+
+     pikachu.apresentarLutas();
+     charmander.apresentarLutas();
+
+
+    // System.out.println("Pikachu Resultado: " + luta.getResultado(pikachu)); 
+    
+
+
+>>>>>>> c0d9003 (Adiciona minha pasta com os exercícios)
     }
 }
